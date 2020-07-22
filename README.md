551fc7e7ba4a9eb342180d6d0a6a0c887f28a276
jerome <jeromebueno55@gmail.com>
2020-07-23 01:10:02 +0200

    Fix perfomMowing and add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# PSE-MowItNow
+# Project Title
+
+MowItNow project in Javascript for Publicis Sapient Engineering
+
+
+## Installation
+```
+yarn install
+```
+
+
+## Getting Started
+
+For perfom mowing, add position of mower and route on array in index.js and run:
+
+```
+yarn run dev
+```
+
+
+## Running the tests
+
+To run the tests launch:
+
+```
+yarn run test
+```
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Monitor = require('./src/monitor')
 
-const instruction = [
-    [5,5],
-    [1,2,'N'],
-    ['G','A','G','A','G','A','G','A','A'],
-    [3,3,'E'],
-    ['A','A','D','A','A','D','A','D','D','A']
+const  instruction = [
+  [5,5],
+  [1,2,'N'],
+  ['G','A','G','A','G','A','G','A','A'],
+  [3,3,'E'],
+  ['A','A','D','A','A','D','A','D','D','A']
 ]
 
-Monitor.mow(instruction)
+Monitor.perfomMowing(instruction)
